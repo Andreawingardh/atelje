3c2598dc95f0c99d49b6ba6b17467d855e9704ec
Jennie-Westerlund <jmn.westerlund@gmail.com>
2025-10-16 21:27:36 +0200

    Add Canvas and imported to designer new

--- frontend/src/features/designer/Canvas3D/Canvas3D.tsx
+++ frontend/src/features/designer/Canvas3D/Canvas3D.tsx
@@ -1,5 +1,17 @@
 import styles from "./Canvas3D.module.css";
-import React, { useRef, useState } from 'react'
+import React from 'react'
 import { Canvas, useFrame } from '@react-three/fiber'
 import * as THREE from 'three'
 
+
+export default function Canvas3D() {
+  return (
+    <section>
+      <Canvas
+        camera={{ position: [5, 5, 5], fov: 50 }}
+        shadows
+      >
+      </Canvas>
+    </section>
+  )
+}
