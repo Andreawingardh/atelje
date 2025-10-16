afa0182300db6fdb02b1d1715ae1de2d1661ffa6
Jennie-Westerlund <jmn.westerlund@gmail.com>
2025-10-16 21:39:28 +0200

    Add test sphere and lighting to canvas

--- frontend/src/features/designer/Canvas3D/Canvas3D.tsx
+++ frontend/src/features/designer/Canvas3D/Canvas3D.tsx
@@ -1,16 +1,29 @@
 import styles from "./Canvas3D.module.css";
-import React from 'react'
+import React, { useRef, useState } from 'react'
 import { Canvas, useFrame } from '@react-three/fiber'
+import { OrbitControls, Box, Sphere, Plane } from '@react-three/drei'
 import * as THREE from 'three'
 
 
 export default function Canvas3D() {
   return (
-    <section>
+    <section className={styles.designerWindow}>
       <Canvas
         camera={{ position: [5, 5, 5], fov: 50 }}
         shadows
       >
+        <ambientLight intensity={0.5} />
+        <pointLight position={[10, 10, 10]} intensity={1} castShadow />
+        <directionalLight
+          position={[0, 10, 0]}
+          intensity={0.5}
+          castShadow
+          shadow-mapSize-width={1024}
+          shadow-mapSize-height={1024}
+        />
+        <Sphere args={[1, 32, 32]} position={[1.5, 0, 0]} castShadow>
+          <meshStandardMaterial color="#E3DCD3" metalness={0.7} roughness={0.2} />
+        </Sphere>
       </Canvas>
     </section>
   )
