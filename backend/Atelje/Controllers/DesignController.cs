a634d6003ccbff512f6941f7db9fa35a97967348
Josefine <josefineahlstrand@live.se>
2025-10-22 10:40:55 +0200

    Add skeleton for design controller

--- backend/Atelje/Controllers/DesignController.cs
+++ backend/Atelje/Controllers/DesignController.cs
@@ -1,6 +1,27 @@
+// using Atelje.DTOs.Design;
+// using Atelje.Services;
+using Microsoft.AspNetCore.Mvc;
+
 namespace Atelje.Controllers;
 
-public class DesignController
+
+[ApiController]
+[Route("api/[controller]")]
+public class DesignController : ControllerBase
 {
+    // public DesignController(IDesignService designService)
+    // {
+    // }
     
+    [HttpGet(Name = "GetAllDesigns")]
+    public async Task<ActionResult<List<object>>> GetDesigns()
+    {
+        throw new NotImplementedException();
+    }
+
+    [HttpGet("{id}", Name = "GetDesignByID")]
+    public async Task<ActionResult<object>> GetDesign(string id)
+    {
+        throw new NotImplementedException();
+    }
 }
