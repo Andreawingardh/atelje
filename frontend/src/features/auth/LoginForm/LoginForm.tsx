703bd4bcc7030984d5cd5d245b53bfb89bb86f17
Andreawingardh <andreawingardh@gmail.com>
2025-10-22 15:14:10 +0200

    Built login functionality
    Use the login form built in the Authcontext to authenticate users. If failed, produces error messages which are shown in the component.

--- frontend/src/features/auth/LoginForm/LoginForm.tsx
+++ frontend/src/features/auth/LoginForm/LoginForm.tsx
@@ -1,20 +1,62 @@
-import React from "react";
+"use client";
+
+import React, { useState } from "react";
 import styles from "./LoginForm.module.css";
+import { useAuth } from "@/contexts/AuthContext";
+import { ApiError } from "@/api/generated";
+import { redirect } from "next/navigation";
 
 export default function LoginForm() {
+  const { login } = useAuth();
+  const [error, setError] = useState<string | null>(null);
 
-    return (
+  async function handleSubmit(formData: FormData) {
+    const email = formData.get("email")!.toString();
+    const password = formData.get("password")!.toString();
+
+    try {
+      await login(email, password);
+      // redirect('new');
+    } catch (error) {
+      if (error instanceof ApiError) {
+        setError(error.body.errors[0]); // Or however you want to handle it
+      } else {
+        setError("An unexpected error occurred");
+      }
+    }
+  }
+
+  return (
     <>
-      <h1 className={styles.title} >Log in</h1>
-      <form className={styles.loginForm}>
-        <label className={styles.loginLabel} htmlFor="username">Username</label>
-        <input className={styles.loginInput} type="text" id="username" name="username" required />
+      <h1 className={styles.title}>Log in</h1>
+      {error && <p>{error}</p>}
+      <form className={styles.loginForm} action={handleSubmit}>
+        <label className={styles.loginLabel} htmlFor="email">
+          Email
+        </label>
+        <input
+          className={styles.loginInput}
+          type="email"
+          id="email"
+          name="email"
+          required
+          key="email"
+        />
 
-        <label className={styles.loginLabel} htmlFor="password">Password</label>
-        <input className={styles.loginInput} type="password" id="password" name="password" required />
+        <label className={styles.loginLabel} htmlFor="password">
+          Password
+        </label>
+        <input
+          className={styles.loginInput}
+          type="password"
+          id="password"
+          key="password"
+          name="password"
+          required
+        />
 
         <button type="submit">Log in</button>
       </form>
     </>
-    )
+  );
 }
