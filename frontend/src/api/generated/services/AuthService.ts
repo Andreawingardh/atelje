170bc968a72f645771ce26831f754280d0dee778
Jennie-Westerlund <jmn.westerlund@gmail.com>
2025-10-22 10:09:08 +0200

    Add new types generated from backend

---
+++ frontend/src/api/generated/services/AuthService.ts
@@ -0,0 +1,42 @@
+/* generated using openapi-typescript-codegen -- do not edit */
+/* istanbul ignore file */
+/* tslint:disable */
+/* eslint-disable */
+import type { AuthResponseDto } from '../models/AuthResponseDto';
+import type { LoginDto } from '../models/LoginDto';
+import type { RegisterDto } from '../models/RegisterDto';
+import type { CancelablePromise } from '../core/CancelablePromise';
+import { OpenAPI } from '../core/OpenAPI';
+import { request as __request } from '../core/request';
+export class AuthService {
+    /**
+     * @param requestBody
+     * @returns AuthResponseDto OK
+     * @throws ApiError
+     */
+    public static postApiAuthRegister(
+        requestBody: RegisterDto,
+    ): CancelablePromise<AuthResponseDto> {
+        return __request(OpenAPI, {
+            method: 'POST',
+            url: '/api/Auth/register',
+            body: requestBody,
+            mediaType: 'application/json',
+        });
+    }
+    /**
+     * @param requestBody
+     * @returns AuthResponseDto OK
+     * @throws ApiError
+     */
+    public static postApiAuthLogin(
+        requestBody: LoginDto,
+    ): CancelablePromise<AuthResponseDto> {
+        return __request(OpenAPI, {
+            method: 'POST',
+            url: '/api/Auth/login',
+            body: requestBody,
+            mediaType: 'application/json',
+        });
+    }
+}
