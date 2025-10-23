9c0c9d191cd3d60d03588a799c78a32819eac31c
Josefine <josefineahlstrand@live.se>
2025-10-23 14:03:20 +0200

    Add schema for design

--- backend/Atelje/DTOs/Design/CreateDesignDto.cs
+++ backend/Atelje/DTOs/Design/CreateDesignDto.cs
@@ -1,7 +1,16 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Atelje.DTOs.Design;
 
+/// <summary>
+/// Data needed to create a new user
+/// </summary>
 public class CreateDesignDto
 {
+    [Required]
+    [MinLength(3)]
     public required string Name { get; set; }
+    
+    [MaxLength(100)]
     public string? UserId { get; set; }
 }
--- backend/Atelje/DTOs/Design/UpdateDesignDto.cs
+++ backend/Atelje/DTOs/Design/UpdateDesignDto.cs
@@ -1,6 +1,13 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Atelje.DTOs.Design;
 
+/// <summary>
+/// Data needed to update a user
+/// </summary>
 public class UpdateDesignDto
 {
+    [Required]
+    [MaxLength(50)]
     public required string Name { get; set; }
 }
