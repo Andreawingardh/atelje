4a41662a0789521951347c7e50d44c96fb8a2faa
Jennie-Westerlund <jmn.westerlund@gmail.com>
2025-10-23 12:37:11 +0200

    Add ProtectedRoute to /new /edit and /dashboard

--- frontend/src/app/(designer)/new/[id]/edit/page.tsx
+++ frontend/src/app/(designer)/new/[id]/edit/page.tsx
@@ -1,9 +1,11 @@
 "use client";
 
+import { ProtectedRoute } from "@/features/auth/ProtectedRoute/ProtectedRoute";
+
 export default function EditDesignPage() {
   return (
-    <>
+    <ProtectedRoute>
     <h1>Designer 3D-tool editing existing design</h1>
-    </>
+    </ProtectedRoute>
   );
 }
