63d379f95200264a32ed879f251d8dea0d2522d7
Andreawingardh <andreawingardh@gmail.com>
2025-10-30 14:54:24 +0100

    Sets up basically functionality for UserDesigns

--- frontend/src/app/dashboard/page.tsx
+++ frontend/src/app/dashboard/page.tsx
@@ -2,13 +2,15 @@
 
 import { useAuth } from "@/contexts/AuthContext";
 import { ProtectedRoute } from "@/features/auth/ProtectedRoute/ProtectedRoute";
+import UserDesigns from "@/features/user/UserDesigns";
 import UserInfo from "@/features/user/UserInfo";
 
 export default function DashboardPage() {
-    const { user, isLoading } = useAuth();
+  const { user, isLoading } = useAuth();
   return (
     <ProtectedRoute>
-    <UserInfo />
+      <UserDesigns />
+      <UserInfo />
     </ProtectedRoute>
   );
 }
