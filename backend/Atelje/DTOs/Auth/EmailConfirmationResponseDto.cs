fadf443961154aeeac354b0256d670d5cb659797
Andreawingardh <andreawingardh@gmail.com>
2025-11-03 13:52:08 +0100

    Renamed EmailConfirmationDto to EmailConfirmationResponseDto

--- backend/Atelje/DTOs/Auth/EmailConfirmationResponseDto.cs
+++ backend/Atelje/DTOs/Auth/EmailConfirmationResponseDto.cs
@@ -1,6 +1,6 @@
 namespace Atelje.DTOs.Auth;
 
-public class EmailConfirmationDto
+public class EmailConfirmationResponseDto
 {
     public required string Message { get; set; }
     public required bool EmailConfirmed { get; set; }
