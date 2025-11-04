05353bc0c452ceac22b54a61c135b0dacefe6d4a
Josefine <josefineahlstrand@live.se>
2025-11-04 14:43:59 +0100

    Add screenshot DTOs

--- backend/Atelje/DTOs/R2/RequestScreenshotUrlsDto.cs
+++ backend/Atelje/DTOs/R2/RequestScreenshotUrlsDto.cs
@@ -1,6 +1,15 @@
-namespace Atelje.DTOs;
+using System.ComponentModel.DataAnnotations;
 
-public class RequestScreenshotUrlsDto
+namespace Atelje.DTOs.R2
 {
-    
+    public class RequestScreenshotUrlsDto
+    {
+        [Required]
+        [StringLength(200)]
+        public string FullFileName { get; set; }
+        
+        [Required]
+        [StringLength(200)]
+        public string ThumbnailFileName { get; set; }
+    }
 }
--- backend/Atelje/DTOs/R2/ScreenshotUrlsDto.cs
+++ backend/Atelje/DTOs/R2/ScreenshotUrlsDto.cs
@@ -1,6 +1,10 @@
-namespace Atelje.DTOs;
-
-public class ScreenshotUrlsDto
+namespace Atelje.DTOs.R2
 {
-    
+    public class ScreenshotUrlsDto
+    {
+        public string FullSizeUploadUrl { get; set; }
+        public string ThumbnailUploadUrl { get; set; }
+        public string FullSizePublicUrl { get; set; }
+        public string ThumbnailPublicUrl { get; set; }
+    }
 }
