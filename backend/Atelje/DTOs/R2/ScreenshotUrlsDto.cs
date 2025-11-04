78e52840c238a6861f51cd056eac99ba23652262
Josefine <josefineahlstrand@live.se>
2025-11-04 14:59:45 +0100

    Add required to DTO

--- backend/Atelje/DTOs/R2/ScreenshotUrlsDto.cs
+++ backend/Atelje/DTOs/R2/ScreenshotUrlsDto.cs
@@ -2,9 +2,9 @@ namespace Atelje.DTOs.R2
 {
     public class ScreenshotUrlsDto
     {
-        public string FullSizeUploadUrl { get; set; }
-        public string ThumbnailUploadUrl { get; set; }
-        public string FullSizePublicUrl { get; set; }
-        public string ThumbnailPublicUrl { get; set; }
+        public required string FullSizeUploadUrl { get; set; }
+        public required string ThumbnailUploadUrl { get; set; }
+        public required string FullSizePublicUrl { get; set; }
+        public required string ThumbnailPublicUrl { get; set; }
     }
 }
