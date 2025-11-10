72e287224fc828ae6ce40deeb79f9b9e234947b4
Andreawingardh <andreawingardh@gmail.com>
2025-11-10 15:02:04 +0100

    Set up basic architecture of NavBar
    In the navbar, logic checks if the user is logged in and displays different buttons  depending on state: register and login for unauthorized user and profile for authorized users. The functions for clicking on buttons are prepared but not populated with code.

---
+++ frontend/src/features/navigation/NavBar/NavBar.tsx
@@ -0,0 +1,50 @@
+"use client";
+
+import { useAuth } from "@/contexts/AuthContext";
+import styles from "./NavBar.module.css";
+import { useEffect, useState } from "react";
+import { useRouter } from "next/navigation";
+
+export default function NavBar() {
+  const { user } = useAuth();
+  const [status, setStatus] = useState<"unauthorized" | "authorized">(
+    "unauthorized"
+  );
+    const router = useRouter();
+
+  useEffect(() => {
+    if (user) {
+      setStatus("authorized");
+    } else {
+      setStatus("unauthorized");
+    }
+  }, [setStatus, user]);
+
+  function handleLoginClick() {}
+  function handleRegisterClick() {}
+    function handleProfileClick() {
+      router.push("/dashboard")
+  }
+
+  return (
+    <nav className={styles.navBarWrapper}>
+      <h1>Ateljé</h1>
+      {status == "unauthorized" && (
+        <>
+          <button onClick={handleLoginClick} name="loginButton">
+            Sign in
+          </button>
+          <button onClick={handleRegisterClick} name="registerButton">
+            Sign up
+          </button>
+        </>
+      )}
+
+      {status == "authorized" && (
+        <button onClick={handleProfileClick} name="profileButton">
+          Profile
+        </button>
+      )}
+    </nav>
+  );
+}
