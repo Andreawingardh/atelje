06d778c544857c1f936230e934b22d918a3cccd4
Andreawingardh <andreawingardh@gmail.com>
2025-11-10 19:26:36 +0100

    Reformatted code

--- frontend/src/contexts/ModalContext.tsx
+++ frontend/src/contexts/ModalContext.tsx
@@ -1,39 +1,47 @@
-"use client"
+"use client";
 
-import { createContext, Dispatch, SetStateAction, useState, useContext } from "react";
+import {
+  createContext,
+  useState,
+  useContext,
+} from "react";
 
 export interface ModalContextType {
-    currentModal: 'login' | 'register' | null,
-    openModal: (modalType: 'login' | 'register') => void,
-    closeModal: () => void
+  currentModal: "login" | "register" | null;
+  openModal: (modalType: "login" | "register") => void;
+  closeModal: () => void;
 }
 
 const ModalContext = createContext<ModalContextType | undefined>(undefined);
 
-export default function ModalProvider ({ children }: { children: React.ReactNode }) {
-    const [currentModal, setCurrentModal] = useState<'login' | 'register' | null>(null);
+export default function ModalProvider({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const [currentModal, setCurrentModal] = useState<"login" | "register" | null>(
+    null
+  );
 
-    function openModal(modalType: 'login' | 'register') {
-        setCurrentModal(modalType)
-    }
+  function openModal(modalType: "login" | "register") {
+    setCurrentModal(modalType);
+  }
 
-    function closeModal() {
-        setCurrentModal(null);
-    }
+  function closeModal() {
+    setCurrentModal(null);
+  }
 
-      return (
-          <ModalContext.Provider value={{currentModal, openModal, closeModal}}>
+  return (
+    <ModalContext.Provider value={{ currentModal, openModal, closeModal }}>
       {children}
     </ModalContext.Provider>
   );
 }
 
 export function useModal() {
-      const context = useContext(ModalContext);
-      if (!context) {
-        throw new Error("useModal must be used within Modal Context");
-      }
-      return context;
-    }
-  
-
+  const context = useContext(ModalContext);
+  if (!context) {
+    throw new Error("useModal must be used within Modal Context");
+  }
+  return context;
+}
