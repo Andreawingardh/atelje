48d7e605499d2e8e27d2fba4c4ec56fb5223448e
Andreawingardh <andreawingardh@gmail.com>
2025-11-10 18:56:31 +0100

    Set up ModalContext and ModalProvider
    The Modal Context has a state called CurrentModal, which holds the current kind of modal open, at this point login or register. OpenModal() takes the kind of modal as a parameter and sets the current modal to the specified parameter. closeModal() sets the currentModal to null.

---
+++ frontend/src/contexts/ModalContext.tsx
@@ -0,0 +1,37 @@
+import { createContext, Dispatch, SetStateAction, useState, useContext } from "react";
+
+export interface ModalContextType {
+    currentModal: 'login' | 'register' | null,
+    openModal: (modalType: 'login' | 'register') => void,
+    closeModal: () => void
+}
+
+const ModalContext = createContext<ModalContextType | undefined>(undefined);
+
+export default function ModalProvider ({ children }: { children: React.ReactNode }) {
+    const [currentModal, setCurrentModal] = useState<'login' | 'register' | null>(null);
+
+    function openModal(modalType: 'login' | 'register') {
+        setCurrentModal(modalType)
+    }
+
+    function closeModal() {
+        setCurrentModal(null);
+    }
+
+      return (
+          <ModalContext.Provider value={{currentModal, openModal, closeModal}}>
+      {children}
+    </ModalContext.Provider>
+  );
+}
+
+export function useModal() {
+      const context = useContext(ModalContext);
+      if (!context) {
+        throw new Error("useModal must be used within Modal Context");
+      }
+      return context;
+    }
+  
+
