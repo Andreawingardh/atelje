9d2f609ac35f2635b564e1bd305f492f300253d2
Jennie-Westerlund <jmn.westerlund@gmail.com>
2025-11-11 21:41:33 +0100

    Uppdate button element with another variant

--- frontend/src/elements/Button/Button.tsx
+++ frontend/src/elements/Button/Button.tsx
@@ -3,22 +3,25 @@ import styles from "./Button.module.css";
 
 export interface ButtonProps
   extends React.ButtonHTMLAttributes<HTMLButtonElement> {
-  variant?: "primary" | "secondary";
+  variant?: "snowdrop" | "terracotta" | "cornflower";
   buttonText?: string;
   buttonIcon?: React.ReactNode;
 }
 
 export default function Button({
-  variant = "primary",
+  variant = "snowdrop",
   buttonText,
   buttonIcon,
   disabled,
   className = "",
   ...props
 }: ButtonProps) {
-  const variantClass =
-    variant === "primary" ? styles.primary : styles.secondary;
-
+    const variantClass = {
+        snowdrop: styles.snowdrop,
+        terracotta: styles.terracotta,
+        cornflower: styles.cornflower,
+      }[variant];
+      
   return (
     <button
       className={`${styles.base} ${variantClass} ${disabled ? styles.disabled : ""} ${className}`}
