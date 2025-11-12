548155dc8f26cce6eed030aeea380359c522134f
Jennie-Westerlund <jmn.westerlund@gmail.com>
2025-11-12 11:17:58 +0100

    Uppdated button styling

--- frontend/src/features/auth/LogOutButton/LogOutButton.tsx
+++ frontend/src/features/auth/LogOutButton/LogOutButton.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { useAuth } from "@/contexts/AuthContext";
+import Button from "@/elements/Button/Button";
 import { useRouter } from "next/navigation";
 
 export default function LogOutButton() {
@@ -12,5 +13,5 @@ export default function LogOutButton() {
     router.push("/login");
   }
 
-  return <button onClick={handleOnClick}>Logout</button>;
+  return <Button onClick={handleOnClick} variant="snowdrop" buttonText="Log out"></Button>;
 }
