904cef317e6abb5398d2785e60948e8ddaa3c4fd
Andreawingardh <andreawingardh@gmail.com>
2025-11-17 13:40:50 +0100

    Removes unused imports

--- frontend/src/features/auth/RegisterForm/RegisterForm.tsx
+++ frontend/src/features/auth/RegisterForm/RegisterForm.tsx
@@ -1,10 +1,9 @@
 "use client";
 
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import styles from "./RegisterForm.module.css";
 import { useAuth } from "@/contexts/AuthContext";
 import { RegisterDto } from "@/api/generated";
-import { useRouter } from "next/navigation";
 import { useModal } from "@/contexts/ModalContext";
 import Button from "@/elements/Button/Button";
 import TextInput from "@/elements/TextInput/TextInput";
