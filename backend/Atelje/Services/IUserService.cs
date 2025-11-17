654b3c7e2071f052de633af924006683f953d860
Andreawingardh <andreawingardh@gmail.com>
2025-11-17 11:27:55 +0100

    Updates UpdateUserAsync to always return UserDto

--- backend/Atelje/Services/IUserService.cs
+++ backend/Atelje/Services/IUserService.cs
@@ -7,7 +7,7 @@ public interface IUserService
     Task<List<UserDto>> GetAllUsersAsync();
     Task<UserDto?> GetUserByIdAsync(string id);
     Task<UserDto> CreateUserAsync(CreateUserDto dto);
-    Task<UserDto?> UpdateUserAsync(string id, UpdateUserDto dto);
+    Task<UserDto> UpdateUserAsync(string id, UpdateUserDto dto);
     Task<bool> DeleteUserAsync(string id);
 
 }
