1f79f41043d4fe27bd0df8d5e82ac69548fd7d8f
Jennie-Westerlund <jmn.westerlund@gmail.com>
2025-11-18 11:17:07 +0100

    Add about us modal on dashboard

--- frontend/src/app/dashboard/page.tsx
+++ frontend/src/app/dashboard/page.tsx
@@ -4,16 +4,24 @@ import { useAuth } from "@/contexts/AuthContext";
 import { ProtectedRoute } from "@/features/auth/ProtectedRoute/ProtectedRoute";
 import UserDesigns from "@/features/user/UserDesigns";
 import UserInfo from "@/features/user/UserInfo";
+import { useModal } from "@/contexts/ModalContext";
 import styles from "./page.module.css";
+import CircleButton from "@/elements/CircleButton/CircleButton";
 
 export default function DashboardPage() {
   const { user, isLoading } = useAuth();
+  const { openModal } = useModal();
   return (
     <>
       <ProtectedRoute>
       <section className={styles.content}>
           <UserDesigns />
           <UserInfo />
+          <CircleButton
+            buttonIcon="./icons/waving-blue-icon.svg"
+            className={styles.aboutUsButton}
+            onClick={() => openModal("about-us")}
+          />
       </section>
       </ProtectedRoute>
     </>
